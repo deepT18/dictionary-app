9faae69e4c8763fffc498b030a7471c47b319632
DeeptenduKuri <deeptendukuri@gmail.com>
2024-02-19 20:33:49 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+
+
+const dictionary=[
+
+  { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." }
+
+]
+
 
 function App() {
+  const [text,setText]=useState("")
+  const [defination,setDefination]=useState("")
+  const handleChange=()=>{
+
+    setDefination("")
+    const searchTerm = text.trim().toLowerCase();
+
+    // Find the word in the dictionary
+    const foundWord = dictionary.find(item => item.word.toLowerCase() === searchTerm);
+
+    if (foundWord) {
+      setDefination(foundWord.meaning);
+    } else {
+      setDefination("Word not found in the dictionary");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dictionary App</h1>
+      <input onChange={(e)=>setText(e.target.value)}/>
+      <button onClick={handleChange}>Search</button>
+      <h3><strong>Defination:</strong></h3>
+      <p>{defination}</p>
     </div>
   );
 }
